f586fc77488194c2fd532a11de6dfc6fceec8195
Andrew Doh <dohandrew@protonmail.com>
2018-10-14 20:55:36 -0700

    finally got working zsh next need theme and terminal.

--- configuration.nix
+++ configuration.nix
@@ -31,41 +31,18 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     vim
+     neovim
      firefox
      git
      weechat
-     stack
      dmenu
-     gnumake
-     gcc
-     python
-     python3
-     binutils-unwrapped
      tldr
    ];
-  #environment.variables = 
- # environment.shells = with pkgs; [zsh];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-  programs.zsh.enable = true;
-  programs.zsh.enableAutosuggestions = true;
-  programs.zsh.enableSyntaxHighlighting = true;
-  programs.zsh.ohMyZsh.enable = true;
   
-  programs.zsh.interactiveShellInit=''
-  export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
-    # Customize your oh-my-zsh options here
-      ZSH_THEME="lambda"
-      plugins=(git)
-
-      source $ZSH/oh-my-zsh.sh
-     '';
-  programs.zsh.promptInit="";
  # user settings
  
  # allow proprietary packages
@@ -74,7 +51,6 @@
     allowUnfree = true;
     };
   
-  # users.defaultUserShell = pkgs.zsh;
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
@@ -90,28 +66,20 @@
   # services.printing.enable = true;
 
   # Enable sound.
-   sound.enable = true;
-   hardware.pulseaudio.enable = true;
+   sound.enable = true; hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
-  #services.xserver.enable = true;
-  #services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
-  #services.xserver.displayManager.lightdm.enable = true;
-  #services.xserver.windowManager.xmonad.enable = true;
   # Enable touchpad support.
   # services.xserver.libinput.enable = true;
 
-  # Enable the KDE Desktop Environment.
-  #services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
   #services.upower.enable = true;
   #systemd.services.upower.enable = true;
-  
   # use xmonad 
   services.xserver = {
 	enable = true;
-	displayManager.lightdm={enable = true;};
+	layout = "us";
+				
 	windowManager.xmonad= {
 		enable = true;
 		enableContribAndExtras = true;
@@ -131,8 +99,18 @@
      uid = 1000;
      home = "/home/andrewdo";
      extraGroups = ["wheel" "networkmanager"];
-     shell = pkgs.zsh;
    };
+
+  users.defaultUserShell = "/run/current-system/sw/bin/zsh";
+
+  programs.zsh = {
+  enable = true;
+  enableAutosuggestions = true;
+  syntaxHighlighting.enable = true;
+  ohMyZsh.enable = true;
+  ohMyZsh.plugins = [ "git" ];
+  ohMyZsh.theme = "lambda";
+  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
